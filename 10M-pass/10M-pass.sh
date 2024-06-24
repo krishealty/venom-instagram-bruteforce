704064f5439ba86257af8fde6742f2e6cf9aa631
devaaaaa509 <xhoming0@gmail.com>
2024-06-24 08:57:03 +0530

    Update 10M-pass.sh
    I fixed error in line 35

--- 10M-pass/10M-pass.sh
+++ 10M-pass/10M-pass.sh
@@ -32,7 +32,7 @@ then
 echo -e  "$ylo >>> exiting... <<<$rset"        
 else
 
-  instagram-py --username $usrnm --password-list $inspass
+  instagram -py --username $usrnm --password-list $inspass
 
 echo
 sleep 30.0
